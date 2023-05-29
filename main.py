9ff4e2bf710971d7d398994e1ce9bd05678d2fb9
sabin <sabina.pauric@student.um.si>
2023-05-29 22:39:41 +0200

    feat: working code for face detection on test video

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import cv2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Read the input image
+#img = cv2.imread('test.png')
+cap = cv2.VideoCapture('IMG_8157.mov')
 
+while cap.isOpened():
+    _, img = cap.read() # Prebere naslednji okvir iz videja in ga shrani v img (_ shrani true/false če uspesno prebrano)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4) # Uporabi detektor obraza face_cascade
+    # delectMultiScale vrne seznam pravokotnikov ki predstavljajo najdene obraze
 
+    for (x, y , w ,h) in faces: #Skozi seznam najdenih obrazov
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3) # Na vsak obraz izrise pravokotnik
+        # x y zgornji levi kot pravokotnika w in h pa širino in višino
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Display the output
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cap.release()
+
